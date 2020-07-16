c9a433b89989ea492c9ab5dcdd05fe4b4686c46b
Abul Kalam Azad <you@example.com>
2020-07-16 21:16:28 +0600

    hooks added and search bar design revamped

--- javascript/nasa-api/src/components/search-bar/SearchBar.js
+++ javascript/nasa-api/src/components/search-bar/SearchBar.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import "./style.css"
 
 const SearchBar = () => {
+    const searchBarInput = useRef(null);
+    const [query, setQuery] = useState("");
+
+    const handleUserQuery = (e) => {
+        setQuery(e.target.value)
+    } 
+    const handleSendingQuery = (e) => {
+        e.preventDefault();
+
+        console.log(query);
+        searchBarInput.current.value = ""
+    } 
     return(
         <div className="search-bar">
-            <input
-                className="search-bar-input"
-                type="text"
-                placeholder="Looking for something .."
-            />
+            <form>
+                <input
+                    className="search-bar-input"
+                    type="text"
+                    onChange={handleUserQuery}
+                    ref={searchBarInput}
+                    placeholder="Looking for something .."
+                />
+                <select className="search-bar-select">
+                    <option value="news">News</option>
+                    <option value="image">Image</option>
+                </select>
+                <button
+                    className="search-bar-button"
+                    type="submit"
+                    onClick={handleSendingQuery}
+                >
+                    Search
+                </button>
+            </form>
         </div>
     )
 }
